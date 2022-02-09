7fd7688d4ca05aa05102ed625702e64b4d56fd36
Gabriel Ravier <gabravier@gmail.com>
2022-02-09 23:25:12 +0100

    fix: Added yet more extra checks in hopes of finding bug...

--- src/malloc.c
+++ src/malloc.c
@@ -18,27 +18,35 @@ static void *do_oom_return(void)
     return (NULL);
 }
 
+static void do_remove_from_list(union my_malloc_block *result,
+    union my_malloc_block **prev_next_location)
+{
+    if (result != NULL) {
+        result->is_used = true;
+        MY_MALLOC_ASSERT(*prev_next_location == result);
+        *prev_next_location = result->next;
+    }
+}
+
 static union my_malloc_block *find_best_block(size_t size)
 {
     union my_malloc_block *result = NULL;
     union my_malloc_block *prev = NULL;
-    union my_malloc_block *prev_before_result = NULL;
+    union my_malloc_block **prev_next_location = NULL;
 
     for (union my_malloc_block *it = g_my_malloc.list_head; it != NULL;
          it = it->next) {
+        MY_MALLOC_ASSERT(it->magic_number == MY_MALLOC_MAGIC_NUMBER);
         MY_MALLOC_ASSERT(it->is_used == false);
-        if (it->size >= size &&
+        if (it->size > size &&
             (result == NULL || result->size <= it->size)) {
-            prev_before_result = prev;
+            prev_next_location = prev ? &prev->next : &g_my_malloc.list_head;
+            MY_MALLOC_ASSERT(*prev_next_location == it);
             result = it;
         }
         prev = it;
     }
-    if (result != NULL) {
-        result->is_used = true;
-        *(prev_before_result ? &prev_before_result->next :
-            &g_my_malloc.list_head) = result->next;
-    }
+    do_remove_from_list(result, prev_next_location);
     return (result);
 }
 
@@ -71,6 +79,7 @@ void *malloc(size_t size)
     if (g_my_malloc.page_size == 0 && !my_malloc_initializer())
         return (do_oom_return());
     result = my_malloc_unlocked(size);
+    MY_MALLOC_ASSERT(((uintptr_t)result % 16) == 0);
     pthread_mutex_unlock(&g_my_malloc.mutex);
     MY_MALLOC_DEBUG_PRINTF("Malloc returning %p\n", result);
     return (result);
